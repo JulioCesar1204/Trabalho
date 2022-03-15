ff6b8e3f67ca938d2511e721e3bc2e807c65e670
Luwx <33076480+Luwx@users.noreply.github.com>
2022-03-15 20:11:04 -0300

    Update programa2.c

--- programa2.c
+++ programa2.c
@@ -1,108 +1,129 @@
+
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct Noh{
+typedef struct Noh
+{
     int valor;
     struct Noh *prox;
 } Noh;
-typedef Noh* NohPtr;
-
+typedef Noh *NohPtr;
 
 NohPtr inserirElemento(int valor, NohPtr inicio)
 {
-    NohPtr np, i, j;
-    if(inicio==NULL)
+    NohPtr np, i, j = NULL;
+    if (inicio == NULL)
     {
-        np= (NohPtr)malloc(sizeof(Noh));
-        (*np).valor=valor;
-        (*np).prox=NULL;
-        inicio=np;
-    }else
+        np = (NohPtr)malloc(sizeof(Noh));
+        (*np).valor = valor;
+        (*np).prox = NULL;
+        inicio = np;
+    }
+    else
     {
-        i=inicio;
-        np=(NohPtr)malloc(sizeof(Noh));
-        (*np).valor=valor;
-        while(valor>(*i).valor)
+        i = inicio;
+        np = (NohPtr)malloc(sizeof(Noh));
+        (*np).valor = valor;
+        while (valor > (*i).valor)
         {
-            j=i;
-            i=(*i).prox;
+            j = i;
+            i = (*i).prox;
+            if (i == NULL)
+                break;
         }
-        (*j).prox=np;
-        (*np).prox=i;
+        if (j != NULL)
+        {
+            (*j).prox = np;
+        } else {
+            inicio = np;
+        }
+        (*np).prox = i;
     }
     return inicio;
-
 }
 
 NohPtr removerElemento(int valor, NohPtr inicio)
 {
     NohPtr i, j;
-    i=inicio;
-    if(inicio==NULL)
+    i = inicio;
+    if (inicio == NULL)
     {
         return NULL;
-    }else
+    }
+    else
     {
-        if((*inicio).prox==valor)
+        if ((*inicio).valor == valor)
         {
-            i=(*inicio).prox;
+            i = (*inicio).prox;
             free(inicio);
             return i;
-        }else
-        {
-            while((*i).valor=!valor)
-            {
-                i=(*i).prox;
-            }
-                j=(*i).prox;
-                (*i).prox=(*j).prox;
-                free(j);
-                return inicio;
         }
+        else
+        {
+            while ((*i).valor = !valor)
+            {
+                i = (*i).prox;
+            }
+            j = (*i).prox; 
+            (*i).prox = (*j).prox; 
+            free(j);
+            return inicio;
+        }
+    }
 }
 
 void exibirElementos(NohPtr inicio)
 {
+    if (inicio == NULL)
+    {
+        printf("\n\nLista vazia");
+        return;
+    }
+
     NohPtr p;
     p = inicio;
-    while(p=!NULL)
+    printf("\n\nLista: ");
+    while (p != NULL)
     {
-        printf("%d\n", (*p).valor);
+        printf("%d ", (*p).valor);
         p = (*p).prox;
     }
     printf("\n");
 }
 
-int main() {
+int main()
+{
     int operacao, elemento;
     NohPtr inicio;
     inicio = NULL;
 
     do
     {
-    printf("Digite um numero de acordo com a operacao que deseja realizar\n0-Encerrar o programa\n1-Exibir todos os elementos da lista\n2-Inserir elemento\n3- Remover elemento\n ");
-    scanf("%d", &operacao);
+        printf("\n\n\nDigite um numero de acordo com a operacao que deseja realizar\n0-Encerrar o programa\n1-Exibir todos os elementos da lista\n2-Inserir elemento\n3- Remover elemento\n ");
+        scanf("%d", &operacao);
 
-    switch(operacao)
-    {
+        switch (operacao)
+        {
+        case 0:
+            return 0;
         case 1:
-            exebirLista(inicio);
+            exibirElementos(inicio);
             break;
         case 2:
             printf("Digite o elemento que deseja inserir:");
             scanf("%d", &elemento);
-            inserirElemento(elemento, inicio);
+            inicio = inserirElemento(elemento, inicio);
             break;
         case 3:
             printf("Digite o elemento que deseja excluir");
             scanf("%d", &elemento);
             removerElemento(elemento, inicio);
+            break;
         default:
             printf("O numero nao corresponde a nenhuma operacao.");
-    }
+        }
 
-    }
-    while(operacao=!0);
+    } while (operacao != 0);
     return 0;
 }
 
